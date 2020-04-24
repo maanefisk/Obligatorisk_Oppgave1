ee439d8af3f2964f2d55620635f45210b49d9117
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-04-24 15:35:26 +0200

    Oblig 1 working

--- lib.js
+++ lib.js
@@ -1,56 +1,60 @@
 
 
-//function loginput() {
-//    console.log(textbox.value)
-//    console.log(str.length)
-//    console.log(str.length)
-//    console.log(str.length)
-//  }
 
-function allchecked(assert) {
-  return checklength(assert) &&
-  checkdots(assert) &&
-  checkyear(assert);
-}
 
-function checklength(assert) {
-  var str = textbox.value;
-  if (str.length == 10) {
-    return true;
-  }
-  }
-
-function checkdots(assert) {
-  var str = textbox.value;
-  var dotdot = str.substring(2,3) + str.substring(5,6);
-  if (dotdot = "..") {
-    return true;
-  }
-  }
-
-function checkyear(assert) {
-var str = textbox.value;
-var year = str.substring(6,10);
-console.log(year);
-if (year.length === 4 && year >= '0000' && year <= '9999') {
-  return true;
-}
-}
-
-function checkmonth(assert) {
-  var mon = textbox.value;
-  var month = mon.substring(3,5);
-  console.log(month);
-  if (month.length === 2 && month >= '01' && month <= '12') {
-    return true;
-  }
+//logger informasjonen på onchange
+function loginput() {
+console.log(textbox.value);
   }
   
-  function checkday(assert) {
-    var da = textbox.value;
-    var day = da.substring(0,2);
-    console.log(day);
-    if (day.length === 2 && day >= '01' && day <= '28') {
-      return true;
+
+// Skjekker alle sammen
+function allchecked(dato) {
+    return checklength(dato) &&
+    checkdots(dato) &&
+    checkyear(dato) &&
+    checkmonth(dato) &&
+    checkday(dato);
+  }
+  
+  // skjekker lengden
+  function checklength(dato) {
+    return dato.length == 10
     }
+  
+    // skjekker punktum
+  function checkdots(dato) {
+    return dato[2]=='.' && dato[5] == '.';
     }
+  
+    // skjekker året
+  function checkyear(dato) {
+  var year = dato.substring(6);
+  return year.length === 4 && year >= '0000' && year <= '9999'
+  }
+  
+  // skjekker måneden
+  function checkmonth(dato) {
+    var month = dato.substring(3,5);
+    return month.length === 2 && month >= '01' && month <= '12'
+  }
+  
+  // Gyldig dag er 1 - 28, men;
+// 29 er lov for februar dersom det skulle være skuddår
+// 30 er lov for alle måneder utenom februar
+// 31 er lovlig for januar, mars, mai, juli, august, oktober og desember
+
+  // skjekker dagsdatoen + gyldighet
+  function checkday(dato) {
+    var day = dato.substring(0,2);
+    var month = dato.substring(3,5);
+    var year = dato.substring(6,10);
+    var isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    var isFebruary = month == '02';
+    var isMonthshort = month == '04' || month == '06' || month == '09' || month == '11';
+    console.log(isLeapYear,day,month,year);
+    return day >= '01' && day <= '28' ||
+    day == '29' && ( !isFebruary || isLeapYear ) ||
+    day == '30' && ( !isFebruary ) ||
+    day == '31' && !isFebruary && !isMonthshort;
+      }
